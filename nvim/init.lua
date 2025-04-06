7d2e8cd206184538d8298c8f706f609367425132
owl9527 <0xc3c740bb@gmail.com>
2025-04-06 14:35:24 +0800

    update: neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,37 +1,17 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.wrap = false
 vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 3
 vim.opt.shiftwidth = 3
 vim.opt.signcolumn = "yes"
+vim.opt.laststatus = 3
+vim.opt.scrolloff = 7
+vim.opt.showmode = false
 vim.opt.completeopt:append("menuone")
-vim.opt.completeopt:append("noinsert")
-vim.cmd.colorscheme("habamax")
+vim.opt.completeopt:append("noselect")
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-   callback = function() vim.hl.on_yank() end
-})
-
-vim.lsp.config["clangd"] = {
-   cmd = {
-      "clangd",
-      "--header-insertion=never",
-      "--header-insertion-decorators=0",
-      "--completion-style=bundled",
-      "--function-arg-placeholders=0"
-   },
-   root_markers = { ".clangd", "compile_commands.json", ".git", "CMakeLists.txt" },
-   filetypes = { "cpp" },
-   capabilities = {
-      textDocument = {
-         semanticTokens = {
-            multilineTokenSupport = true
-         }
-      }
-   }
-}
+vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.hl.on_yank() end })
 
 vim.api.nvim_create_autocmd("LspAttach", {
    callback = function(args)
@@ -42,4 +22,45 @@ vim.api.nvim_create_autocmd("LspAttach", {
    end
 })
 
-vim.lsp.enable("clangd")
+vim.lsp.config("*", {
+   capabilities = {
+      textDocument = {
+         semanticTokens = {
+            multilineTokenSupport = true
+         }
+      }
+   }
+})
+
+vim.lsp.config.pyright = {
+   cmd = { "pyright-langserver", "--stdio" },
+   root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json", ".git" },
+   filetypes = { "python" },
+   settings = {
+      python = {
+         analysis = {
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+            diagnosticMode = "openFilesOnly"
+         }
+      }
+   }
+}
+
+vim.lsp.config.clangd = {
+   cmd = {
+      "clangd",
+      "--header-insertion=never",
+      "--header-insertion-decorators=0",
+      "--completion-style=bundled",
+      "--function-arg-placeholders=0"
+   },
+   root_markers = { ".clangd", "compile_commands.json", ".git", "CMakeLists.txt" },
+   filetypes = { "c", "cpp" }
+}
+
+vim.lsp.enable({ "clangd", "pyright" })
+vim.diagnostic.config({ virtual_text = { current_line = true }})
+
+-- 3rd party
+vim.cmd.colorscheme("nightfox")
